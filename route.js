a2b7ed3ec44013755ef73f11d5cacbfa7b5d1ef6
Alexey Sachevichik <roim@ya.ru>
2019-06-12 15:46:37 +0300

    Check JSON parse

--- route.js
+++ route.js
@@ -16,8 +16,7 @@ module.exports = function(app){
       auth: {
         user: request.body.user,
         pass: request.body.pass
-      },
-      pool: true
+      }
     });
 
     var sendSingleMail = function(sender, name, lead, address, subject, text){
@@ -66,8 +65,6 @@ module.exports = function(app){
         console.log(error);
         return response.sendStatus(500);
       });
-
-    transporter.close();
   });
 
   app.post("/test", (request, response) => {
